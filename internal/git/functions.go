983aa45825ecd11cd2a68f8e5e96d095f0a3c53d
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-04 17:20:46 +0100

    [TASK] added CheckoutBranch and PullLatest function

--- internal/git/functions.go
+++ internal/git/functions.go
@@ -1,1 +1,20 @@
 package git
+
+import (
+    "os"
+    "os/exec"
+)
+
+func CheckoutBranch(dir, branch string) error {
+    cmd := exec.Command("git", "checkout", "--force", "--quiet", branch)
+    cmd.Dir = dir
+    cmd.Stdout = os.Stdout
+    return cmd.Run()
+}
+
+func PullLatest(dir string) error {
+    cmd := exec.Command("git", "pull", "--quiet")
+    cmd.Dir = dir
+    cmd.Stdout = os.Stdout
+    return cmd.Run()
+}
