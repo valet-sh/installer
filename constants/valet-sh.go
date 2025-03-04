11b2c0c8436655f0983685e46aa0e734eae88e10
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-04 17:19:49 +0100

    [TASK] added constants package

---
+++ constants/valet-sh.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+    ValetBasePath     = "/usr/local/valet-sh/valet-sh"
+    NextBranchFile    = ".next_branch"
+    RuntimeFileName   = ".runtime_version"
+    VersionFileName   = ".version"
+)
