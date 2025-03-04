b1637b0db41ffc77715442f3660e148c3f720aaa
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-04 17:21:54 +0100

    [TASK] updated cmd/update.go

--- cmd/update.go
+++ cmd/update.go
@@ -7,14 +7,16 @@ import (
     "github.com/spf13/cobra"
     "github.com/charmbracelet/huh"
 
-    //"github.com/valet-sh/valet-sh-updater/internal/git"
-    //"github.com/valet-sh/valet-sh-updater/internal/runtime"
+    "github.com/valet-sh/valet-sh-updater/constants"
+    "github.com/valet-sh/valet-sh-updater/internal/git"
+    "github.com/valet-sh/valet-sh-updater/internal/runtime"
 )
 
 var updateCmd = &cobra.Command{
     Use:  "update",
     Short: "Update valet-sh to the latest version",
     Long: `Update valet-sh to the latest version`,
+    SilenceUsage: true,
     RunE: func(cmd *cobra.Command, args []string) error {
         return runUpdate()
     },
@@ -33,7 +35,19 @@ func runUpdate() error {
     if err != nil {
      return err
     }
-    fmt.Println("Updating valet-sh to the latest version on the " + branch + " branch")
+
+    repoPath := constants.ValetBasePath
+    if err := checkIfRepoExists(repoPath); err != nil {
+        return err
+    }
+
+    if branch == "next" {
+        return updateNextBranch(repoPath)
+    } else {
+        return nil
+    }
+
+    fmt.Println("Just a test - branch:", branch)
     return nil
 }
 
@@ -42,7 +56,7 @@ func determineBranch() (string, error) {
         return branchFlag, nil
     }
 
-    _, err := os.Stat("/usr/local/valet-sh/etc/ENABLE_NEXT")
+    _, err := os.Stat(constants.NextBranchFile)
     if err == nil {
         return "next", nil
     }
@@ -66,4 +80,25 @@ func determineBranch() (string, error) {
     }
 
     return selectedBranch, nil
+}
+
+func checkIfRepoExists(repoPath string) error {
+    _, err := os.Stat(repoPath)
+    if os.IsNotExist(err) {
+        return fmt.Errorf("valet-sh not found in %s", repoPath)
+    }
+    return nil
+}
+
+func updateNextBranch(repoPath string) error {
+    fmt.Println("Updating valet-sh to the latest version on the next branch")
+
+    if err := git.CheckoutBranch(repoPath, "next"); err != nil {
+        return fmt.Errorf("Failed to checkout next branch: %w", err)
+    }
+
+    if err := git.PullLatest(repoPath); err != nil {
+        return fmt.Errorf("Failed to pull latest changes: %w", err)
+    }
+    return runtime.CheckRuntime()
 }
