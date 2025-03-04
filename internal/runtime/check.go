0495f17955cc33003879c8c8531cea1f57108e93
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-04 17:21:16 +0100

    [TASK] added CheckRuntime function

--- internal/runtime/check.go
+++ internal/runtime/check.go
@@ -1,1 +1,41 @@
 package runtime
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+
+    "github.com/valet-sh/valet-sh-updater/constants"
+)
+
+func CheckRuntime() error {
+    fmt.Println("Checking runtime")
+
+    runtimePath := filepath.Join(constants.ValetBasePath, constants.RuntimeFileName)
+    versionPath := filepath.Join(constants.ValetBasePath, constants.VersionFileName)
+
+    for _, path := range []string{runtimePath, versionPath} {
+        fmt.Printf("Check if '%s' exists\n", path)
+        if _, err := os.Stat(path); err != nil {
+            if os.IsNotExist(err) {
+                return fmt.Errorf("file %s does not exist", path)
+            }
+            return fmt.Errorf("error checking file %s: %w", path, err)
+        }
+    }
+
+    runtimeVersion, err := os.ReadFile(runtimePath)
+    if err != nil {
+        return fmt.Errorf("failed to read runtime version: %w", err)
+    }
+
+    version, err := os.ReadFile(versionPath)
+    if err != nil {
+        return fmt.Errorf("failed to read version: %w", err)
+    }
+
+    fmt.Printf("Runtime version: %s\n", runtimeVersion)
+    fmt.Printf("Version: %s\n", version)
+
+    return nil
+}
