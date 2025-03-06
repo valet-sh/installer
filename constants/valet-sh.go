3bb30209e8da3c7d65668c7de37c086ec625c1ac
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-06 13:09:12 +0100

    [TASK] updated constants/valet-sh.go

--- constants/valet-sh.go
+++ constants/valet-sh.go
@@ -3,7 +3,9 @@ package constants
 const (
     ValetBasePath     = "/usr/local/valet-sh/valet-sh"
     ValetVenvPath     = "/usr/local/valet-sh/venv"
-    ValetEtcPath     = "/usr/local/valet-sh/etc"
+    ValetEtcPath      = "/usr/local/valet-sh/etc"
+    ValetVenvTmpPath  = "/usr/local/valet-sh/venv-tmp"
+    ValetPath         = "/usr/local/valet-sh"
     NextBranchFile    = "ENABLE_NEXT"
     RuntimeFileName   = ".runtime_version"
     VersionFileName   = ".version"
