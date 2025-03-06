67dcda0aa1c2f8fdca0a87dfd95189d7b89fc866
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-06 16:13:37 +0100

    [TASK] added functions got internal/git/functions.go

--- internal/git/functions.go
+++ internal/git/functions.go
@@ -3,6 +3,11 @@ package git
 import (
     "os"
     "os/exec"
+    "regexp"
+    "sort"
+    "strings"
+
+    "golang.org/x/mod/semver"
 )
 
 func CheckoutBranch(dir, branch string) error {
@@ -17,4 +22,57 @@ func PullLatest(dir string) error {
     cmd.Dir = dir
     cmd.Stdout = os.Stdout
     return cmd.Run()
+}
+
+func FetchTags(dir string) error {
+    cmd := exec.Command("git", "fetch", "--tags", "--quiet")
+    cmd.Dir = dir
+    return cmd.Run()
+}
+
+func GetCurrentReleaseTag(dir string) (string, error) {
+    cmd := exec.Command("git", "describe", "--tags")
+    cmd.Dir = dir
+    out, err := cmd.Output()
+    if err != nil {
+        return "", err
+    }
+    return strings.TrimSpace(string(out)), nil
+}
+
+func FilterTagsSemver(tags []string, pattern string) []string {
+    re := regexp.MustCompile(pattern)
+    var filtered []string
+
+    for _, tag := range tags {
+        if re.MatchString(tag) {
+            vTag := tag
+            if !strings.HasPrefix(vTag, "v") {
+                vTag = "v" + vTag
+        }
+        filtered = append(filtered, vTag)
+        }
+    }
+
+    sort.Slice(filtered, func(i, j int) bool {
+        return semver.Compare(filtered[i], filtered[j]) > 0
+    })
+
+    return filtered
+}
+
+func GetAllTags(dir string) ([]string, error) {
+    cmd := exec.Command("git", "tag")
+    cmd.Dir = dir
+    out, err := cmd.Output()
+    if err != nil {
+        return nil, err
+    }
+
+    tags := strings.Split(string(out), "\n")
+    return tags[:len(tags)-1], nil
+}
+
+func CompareReleases(current, latest string) bool {
+    return semver.Compare(current, latest) < 0
 }
