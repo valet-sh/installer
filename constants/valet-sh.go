6927dca144c3e70160d45410a7130fba5ad16b71
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-07 15:42:48 +0100

    [TASK] added MajorReleaseFile and ValetMajorVersion to constants

--- constants/valet-sh.go
+++ constants/valet-sh.go
@@ -6,7 +6,9 @@ const (
     ValetEtcPath      = "/usr/local/valet-sh/etc"
     ValetVenvTmpPath  = "/usr/local/valet-sh/venv-tmp"
     ValetPath         = "/usr/local/valet-sh"
+    MajorReleaseFile  = "MAJOR_RELEASE_VERSION"
     NextBranchFile    = "ENABLE_NEXT"
     RuntimeFileName   = ".runtime_version"
     VersionFileName   = ".version"
+    ValetMajorVersion = "2"
 )
