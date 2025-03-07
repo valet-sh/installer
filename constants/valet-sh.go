19c9a831aa4108610c9505381019f15127f5e173
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-07 18:48:30 +0100

    [TASK] updated constants/valet-sh.go

--- constants/valet-sh.go
+++ constants/valet-sh.go
@@ -1,14 +1,14 @@
 package constants
 
 const (
-    ValetBasePath     = "/usr/local/valet-sh/valet-sh"
-    ValetVenvPath     = "/usr/local/valet-sh/venv"
-    ValetEtcPath      = "/usr/local/valet-sh/etc"
-    ValetVenvTmpPath  = "/usr/local/valet-sh/venv-tmp"
-    ValetPath         = "/usr/local/valet-sh"
-    MajorReleaseFile  = "MAJOR_RELEASE_VERSION"
-    NextBranchFile    = "ENABLE_NEXT"
-    RuntimeFileName   = ".runtime_version"
-    VersionFileName   = ".version"
-    ValetMajorVersion = "2"
+    ValetBasePath      = "/usr/local/valet-sh/valet-sh"
+    ValetVenvPath      = "/usr/local/valet-sh/venv"
+    ValetEtcPath       = "/usr/local/valet-sh/etc"
+    ValetVenvTmpPath   = "/usr/local/valet-sh/venv-tmp"
+    ValetPath          = "/usr/local/valet-sh"
+    ReleaseChannelFile = "RELEASE_CHANNEL"
+    NextBranchFile     = "ENABLE_NEXT"
+    RuntimeFileName    = ".runtime_version"
+    VersionFileName    = ".version"
+    ValetStableVersion  = "2.x"
 )
