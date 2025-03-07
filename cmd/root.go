a8e3c8750ebdc8920c5352611d463fc713552421
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-07 11:51:54 +0100

    [TASK] added preflightChecks function to the root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,21 @@
 package cmd
 
 import (
+    "fmt"
+
     "github.com/spf13/cobra"
 )
+
+func preflightChecks(cmd *cobra.Command, args []string) error {
+    fmt.Println("Running preflight checks now after root command")
+    return nil
+}
 
 var rootCmd = &cobra.Command{
     Use:   "valet-sh-installer",
     Short: "A CLI tool to update Valet-sh",
     Long: `A CLI tool to update Valet-sh`,
+    PersistentPreRunE: preflightChecks,
 }
 
 func Execute() error {
