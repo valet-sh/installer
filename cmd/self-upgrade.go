986376ead64a3d358afa305d7dbdc024a8d9eb0f
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-09 17:20:00 +0100

    [WIP] added initial example code for self-upgrade from creativeprojects repo

--- cmd/self-upgrade.go
+++ cmd/self-upgrade.go
@@ -2,8 +2,14 @@ package cmd
 
 import (
     "fmt"
+    "log"
+    "errors"
+    "context"
+    "runtime"
+
 
     "github.com/spf13/cobra"
+    "github.com/creativeprojects/go-selfupdate"
 )
 
 var selfUpgradeCmd = &cobra.Command{
@@ -12,14 +18,37 @@ var selfUpgradeCmd = &cobra.Command{
     Long: `Update valet-sh-installer to the latest version`,
     SilenceUsage: true,
     RunE: func(cmd *cobra.Command, args []string) error {
-        return selfUpgrade()
+        return selfUpgrade(rootCmd.Version)
     },
 }
 
 func init() {
 }
 
-func selfUpgrade() error {
-    fmt.Println("self-upgrade")
+func selfUpgrade(version string) error {
+    latest, found, err := selfupdate.DetectLatest(context.Background(), selfupdate.ParseSlug("valet-sh/valet-sh-installer"))
+    fmt.Println(latest)
+    if err != nil {
+        return fmt.Errorf("error occurred while detecting version: %w", err)
+    }
+    if !found {
+        return fmt.Errorf("latest version for %s/%s could not be found from github repository", runtime.GOOS, runtime.GOARCH)
+    }
+
+    if latest.LessOrEqual(version) {
+        log.Printf("Current version (%s) is the latest", version)
+        return nil
+    }
+
+    exe, err := selfupdate.ExecutablePath()
+    if err != nil {
+        return errors.New("could not locate executable path")
+    }
+    if err := selfupdate.UpdateTo(context.Background(), latest.AssetURL, latest.AssetName, exe); err != nil {
+        return fmt.Errorf("error occurred while updating binary: %w", err)
+    }
+    log.Printf("Successfully updated to version %s", latest.Version())
     return nil
 }
+
+
