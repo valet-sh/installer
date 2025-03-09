cbb6a91e655409dbb449a4b253e2bae24eb6d335
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-09 17:22:19 +0100

    [TASK] renamed constants to Vsh prefix and added constants from install script

--- constants/valet-sh.go
+++ constants/valet-sh.go
@@ -1,14 +1,26 @@
 package constants
 
 const (
-    ValetBasePath      = "/usr/local/valet-sh/valet-sh"
-    ValetVenvPath      = "/usr/local/valet-sh/venv"
-    ValetEtcPath       = "/usr/local/valet-sh/etc"
-    ValetVenvTmpPath   = "/usr/local/valet-sh/venv-tmp"
-    ValetPath          = "/usr/local/valet-sh"
+    VshBasePath      = "/usr/local/valet-sh/valet-sh"
+    VshVenvPath      = "/usr/local/valet-sh/venv"
+    VshEtcPath       = "/usr/local/valet-sh/etc"
+    VshVenvTmpPath   = "/usr/local/valet-sh/venv-tmp"
+    VshPath          = "/usr/local/valet-sh"
     ReleaseChannelFile = "RELEASE_CHANNEL"
     NextBranchFile     = "ENABLE_NEXT"
     RuntimeFileName    = ".runtime_version"
     VersionFileName    = ".version"
     ValetStableVersion  = "2.x"
+
+    VshUrl               = "https://valet.sh"
+    VshPrefix            = "/usr/local"
+    HomebrewPrefix       = "/usr/local"
+    VshInstallLog        = "/tmp/valet-sh-install.log"
+    VshGithubNamespace   = "valet-sh"
+    VshGithubRepoName    = "valet-sh"
+    VshGithubRepoUrl     = "https://github.com/valet-sh/valet-sh"
+    VshIncludeUrl        = "https://raw.githubusercontent.com/valet-sh/install/master/include.sh"
+    VshAnsibleFactsFile  = "/tmp/ansible-facts/local"
+    VshInstallPath       = "/usr/local/valet-sh"
+    VshAnsibleRequirementsFile = "/usr/local/valet-sh/valet-sh/requirements.yml"
 )
