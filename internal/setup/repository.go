46f73a705f01df61716009a188678e2cd3c6777a
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-03-10 15:19:47 +0100

    [TASK] updated internal/setup/repository.go

--- internal/setup/repository.go
+++ internal/setup/repository.go
@@ -7,7 +7,6 @@ import (
 
     "github.com/valet-sh/valet-sh-installer/constants"
     "github.com/valet-sh/valet-sh-installer/internal/git"
-    "github.com/valet-sh/valet-sh-installer/internal/utils"
 )
 
 func SetupRepository(logFile *os.File) error {
@@ -21,10 +20,6 @@ func SetupRepository(logFile *os.File) error {
     if _, err := os.Stat(filepath.Join(constants.VshBasePath, ".git")); os.IsNotExist(err) {
         if err := git.CloneRepository(constants.VshGithubRepoUrl, constants.VshBasePath); err != nil {
             return fmt.Errorf("failed to clone repository: %w", err)
-        }
-    } else {
-        if err := utils.RunCommand("git", []string{"-C", constants.VshBasePath, "pull"}, logFile); err != nil {
-            return fmt.Errorf("failed to update repository: %w", err)
         }
     }
     return nil
