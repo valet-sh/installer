a5f0f5656d91265d0cf42b092bd3445fa28d74b2
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-04-07 14:07:41 +0200

    [TASK] fix untar function to preserve symlinks

--- internal/utils/tar.go
+++ internal/utils/tar.go
@@ -1,55 +1,60 @@
 package utils
 
 import (
-    "archive/tar"
-    "compress/gzip"
-    "io"
-    "os"
-    "path/filepath"
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"os"
+	"path/filepath"
 )
 
 func Untar(dst string, r io.Reader) error {
-    gzr, err := gzip.NewReader(r)
-    if err != nil {
-        return err
-    }
-    defer gzr.Close()
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
 
-    tr := tar.NewReader(gzr)
+	tr := tar.NewReader(gzr)
 
-    for {
-        header, err := tr.Next()
+	for {
+		header, err := tr.Next()
 
-        switch {
-        case err == io.EOF:
-            return nil
-        case err != nil:
-            return err
-        case header == nil:
-            continue
-        }
-        target := filepath.Join(dst, header.Name)
+		switch {
+		case err == io.EOF:
+			return nil
+		case err != nil:
+			return err
+		case header == nil:
+			continue
+		}
+		target := filepath.Join(dst, header.Name)
 
-        switch header.Typeflag {
+		switch header.Typeflag {
 
-        case tar.TypeDir:
-            if _, err := os.Stat(target); err != nil {
-                if err := os.MkdirAll(target, 0755); err != nil {
-                    return err
-                }
-            }
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
 
-        case tar.TypeReg:
-            f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
-            if err != nil {
-                return err
-            }
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
 
-            if _, err := io.Copy(f, tr); err != nil {
-                return err
-            }
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
 
-            f.Close()
-        }
-    }
+			f.Close()
+
+		case tar.TypeSymlink:
+			if err := os.Symlink(header.Linkname, target); err != nil {
+				return err
+			}
+		}
+	}
 }
