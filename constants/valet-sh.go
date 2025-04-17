7e95e1dbf836414d33af515af97079319f78f1ef
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2025-04-17 17:01:51 +0200

    [TASK] updated formatting

--- constants/valet-sh.go
+++ constants/valet-sh.go
@@ -1,19 +1,19 @@
 package constants
 
 const (
-    VshBasePath      = "/usr/local/valet-sh/valet-sh"
-    VshVenvPath      = "/usr/local/valet-sh/venv"
-    VshEtcPath       = "/usr/local/valet-sh/etc"
-    VshVenvTmpPath   = "/usr/local/valet-sh/venv-tmp"
-    VshPath          = "/usr/local/valet-sh"
-    ReleaseChannelFile = "RELEASE_CHANNEL"
-    RuntimeFileName    = ".runtime_version"
-    VersionFileName    = ".version"
-    ValetStableVersion  = "2.x"
+	VshBasePath        = "/usr/local/valet-sh/valet-sh"
+	VshVenvPath        = "/usr/local/valet-sh/venv"
+	VshEtcPath         = "/usr/local/valet-sh/etc"
+	VshVenvTmpPath     = "/usr/local/valet-sh/venv-tmp"
+	VshPath            = "/usr/local/valet-sh"
+	ReleaseChannelFile = "RELEASE_CHANNEL"
+	RuntimeFileName    = ".runtime_version"
+	VersionFileName    = ".version"
+	ValetStableVersion = "2.x"
 
-    VshUrl               = "https://valet.sh"
-    HomebrewPrefix       = "/usr/local"
-    VshInstallLog        = "/tmp/valet-sh-install.log"
-    VshGithubRepoUrl     = "https://github.com/valet-sh/valet-sh"
-    VshAnsibleFactsFile  = "/tmp/ansible-facts/local"
+	VshUrl              = "https://valet.sh"
+	HomebrewPrefix      = "/usr/local"
+	VshInstallLog       = "/tmp/valet-sh-install.log"
+	VshGithubRepoUrl    = "https://github.com/valet-sh/valet-sh"
+	VshAnsibleFactsFile = "/tmp/ansible-facts/local"
 )
