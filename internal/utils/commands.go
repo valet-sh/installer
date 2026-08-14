24a1e452f4077faf3a02cc06b99a4cc3bb693d77
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2026-08-14 16:22:34 +0200

    refactor: add commands

--- internal/utils/commands.go
+++ internal/utils/commands.go
@@ -1,18 +1,33 @@
 package utils
 
 import (
-    "os"
-    "os/exec"
+	"os"
+	"os/exec"
 )
 
 func RunCommand(command string, args []string) error {
-    cmd := exec.Command(command, args...)
-    cmd.Stdout = LogFile
-    cmd.Stderr = LogFile
-    return cmd.Run()
+	cmd := exec.Command(command, args...)
+	cmd.Stdout = LogFile
+	cmd.Stderr = LogFile
+	return cmd.Run()
+}
+
+func RunInteractiveCommand(command string, args []string) error {
+	return RunInteractiveCommandWithEnv(command, args, nil)
+}
+
+func RunInteractiveCommandWithEnv(command string, args []string, extraEnv []string) error {
+	cmd := exec.Command(command, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if len(extraEnv) > 0 {
+		cmd.Env = append(os.Environ(), extraEnv...)
+	}
+	return cmd.Run()
 }
 
 func PathExists(path string) bool {
-    _, err := os.Stat(path)
-    return err == nil
+	_, err := os.Stat(path)
+	return err == nil
 }
