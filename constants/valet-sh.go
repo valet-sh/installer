ecbc36dbba62987b25c252288b075d5d3f90c5b4
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2026-08-20 11:07:04 +0200

    refactor: add VshMigrationFile constant

--- constants/valet-sh.go
+++ constants/valet-sh.go
@@ -14,6 +14,7 @@ const (
 
 	VshServiceFile      = VshEtcPath + "/services.yml"
 	VshBundlesFile      = VshEtcPath + "/bundles.yml"
+	VshMigrationFile    = VshEtcPath + "/migration.yml"
 	VshUrl              = "https://valet.sh"
 	HomebrewPrefix      = "/usr/local"
 	VshInstallLog       = "/tmp/valet-sh-install.log"
